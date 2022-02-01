6351f1859e709432735f850317fa966f9931265c
Sabina <mms8818@mail.ru>
2022-02-01 20:39:14 +0300

    add example010_metod

---
+++ Example010_metod/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array={1,2,3,8,5,9,6,3,5,4};
+
+int n = array.Length;
+int find = 4;
+
+int index=0;
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
